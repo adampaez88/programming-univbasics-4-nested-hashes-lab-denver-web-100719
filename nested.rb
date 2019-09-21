9c3825fcf4457e40bc64957bff8a19d3c74ac354
Adam chernitsky <adam987@comcast.net>
2019-09-21 02:32:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,7 +126,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-		 programmer_hash = new_dennis
+		 new_dennis = programmer_hash
 		 new_dennis = {
 			 :grace_hopper => {
 				 :known_for => "COBOL",
