dae84d86ae8ac0b5d775d7bc572f5d2df87c8f9a
Adam chernitsky <adam987@comcast.net>
2019-09-21 02:36:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -146,20 +146,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 new_dennis = programmer_hash
-		 new_dennis = {
-			 :grace_hopper => {
-				 :known_for => "COBOL",
-				 :languages => ["COBOL", "FORTRAN"]
-			 },
-			 :alan_kay => {
-				 :known_for => "Object Orientation",
-				 :languages => ["Smalltalk", "LISP"]
-			 },
-			 :dennis_ritchie => {
-				 :known_for => "Unix",
-				 :languages => ["C"]
-		 }
-new_dennis
+
 
 end
